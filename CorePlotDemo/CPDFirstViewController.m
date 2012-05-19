87c9d6891e73b0c619b14a8906ca1c30fe12223f
Fahim Farook <fahimf@gmail.com>
2012-05-19 10:17:50 +0800

    Initial Commit

---
+++ CorePlotDemo/CPDFirstViewController.m
@@ -0,0 +1,34 @@
+//
+//  CPDFirstViewController.m
+//  CorePlotDemo
+//
+//  Created by Fahim Farook on 19/5/12.
+//  Copyright (c) 2012 RookSoft Pte. Ltd. All rights reserved.
+//
+
+#import "CPDFirstViewController.h"
+
+@interface CPDFirstViewController ()
+
+@end
+
+@implementation CPDFirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
---
+++ CorePlotDemo/CPDSecondViewController.m
@@ -0,0 +1,34 @@
+//
+//  CPDSecondViewController.m
+//  CorePlotDemo
+//
+//  Created by Fahim Farook on 19/5/12.
+//  Copyright (c) 2012 RookSoft Pte. Ltd. All rights reserved.
+//
+
+#import "CPDSecondViewController.h"
+
+@interface CPDSecondViewController ()
+
+@end
+
+@implementation CPDSecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
